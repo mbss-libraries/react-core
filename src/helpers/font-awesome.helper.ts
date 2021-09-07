4b9c0275ed6ec9947ab4d7b1dd82f663e852fcf9
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-09-07 20:06:10 +0200

    ...

--- src/helpers/font-awesome.helper.ts
+++
@@ -1,5 +0,0 @@
-import _ from 'lodash';
-import * as icons from '@fortawesome/free-solid-svg-icons';
-
-export const fasIcons: string[] = _.map(icons, (icon) => icon['iconName']);
-export const isFasIcon = (value: string | undefined) => value !== undefined && fasIcons.includes(value);
