77053fe477c385e44653f58c77e3a0d05cc795c3
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-09-10 20:21:59 +0200

    [COMPONENTS] Add colors to UIButton

--- src/components/ui-elements/ui-button/UIButton.tsx
+++ src/components/ui-elements/ui-button/UIButton.tsx
@@ -1,23 +1,24 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Button, SemanticSIZES } from 'semantic-ui-react';
+import { Button, SemanticCOLORS, SemanticSIZES } from 'semantic-ui-react';
 import '@fortawesome/fontawesome-free';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'semantic-ui-css/components/button.min.css';
 
 export interface UIButtonProps {
   size?: SemanticSIZES;
+  color?: SemanticCOLORS | 'facebook' | 'google plus' | 'vk' | 'twitter' | 'linkedin' | 'instagram' | 'youtube';
   leftIcon?: IconProp;
   leftIconStyle?: React.CSSProperties;
   children?: React.ReactNode;
 }
 export const UIButton: React.FC<UIButtonProps> = (props: React.PropsWithChildren<UIButtonProps>) => {
-  const { size, leftIcon, leftIconStyle } = props;
+  const { size, color, leftIcon, leftIconStyle, children } = props;
   return (
-    <Button size={size}>
+    <Button size={size} color={color}>
       {leftIcon && <FontAwesomeIcon icon={leftIcon} style={{ marginRight: '1rem', ...leftIconStyle }} />}
-      {props.children}
+      {children}
     </Button>
   );
 };
