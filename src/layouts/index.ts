8031824c79e0b4333f739503ba377ec26b56f9f7
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-09-20 14:45:22 +0200

    Update index.ts

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -4,3 +4,4 @@ export * from './layout-blank/LayoutBlank';
 export * from './sidenav/Sidenav';
 export * from './sidenav-item/SidenavItem';
 export * from './header/Header';
+export * from './page-header/PageHeader';
